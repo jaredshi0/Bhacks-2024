edd38b644d35c699da1df34195a456ba3736ad1d
Jared <jaredshi0@gmail.com>
2024-11-02 18:24:32 -0400

    finish index page a nd added fonts

--- frontend/app/RecipePage.jsx
+++ frontend/app/RecipePage.jsx
@@ -1,10 +1,28 @@
 import { Text, View, StyleSheet, ScrollView, Button, Pressable} from "react-native";
 import {router} from 'expo-router'
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import {useLocalSearchParams} from 'expo-router'
+import {
+  SourceSerifPro_400Regular,
+} from '@expo-google-fonts/source-serif-pro'
+
+
 export default function RecipePage()
 {
+	let [fontsLoaded] = useFonts({
+		SourceSerifPro_400Regular,
+		'InstrumentSans': require('../assets/fonts/InstrumentSans.ttf'),
+	})
+
+
     const params = useLocalSearchParams();
     const {recipe_Name,ingredients, directions} = params;
+		
+		if (!fontsLoaded)
+		{
+			return <AppLoading/>;
+		}else{
     return(    
     <View style = {style.pageStyle}>
 
@@ -20,9 +38,12 @@ export default function RecipePage()
 			<View style = {style.directions}>
 				<Text style = {style.directionText}>{directions}</Text>
 			</View>
-
+			<Pressable style={style.buttonStyle} onPress={() => console.log("for backend")}>
+            <Text style={style.buttonText}> Save Recipe </Text>
+      </Pressable>
     </View>
     )
+		}
 }
 
 const style = StyleSheet.create(
@@ -46,34 +67,57 @@ const style = StyleSheet.create(
 		recipeName:
 		{
 			flex:0,
-			padding:30.
+			paddingLeft:20,
+			paddingTop:20,
 		},
 		recipeNameText:
 		{
 			fontSize:36,
+			fontFamily:'SourceSerifPro_400Regular'
 		},
 		ingredients:
 		{
 			flex: 0,
 			alignSelf:'flex-start',
-			marginLeft:20,
-			padding:10,
+			marginLeft:5,
+			padding:20,
 		},
 		ingredientsText:
 		{
-				fontSize:16
+				fontSize:16,
+				fontFamily:'InstrumentSans',
+				fontWeight:'600',
+
 		},
 		directions:
 		{
 			flex: 0,
 			alignSelf:'flex-start',
-			padding:10,
-			marginLeft:20,
+			padding:20,
+			marginLeft:5,
 
 		},
 		directionText:
 		{
-			fontSize:16
+			fontSize:16,
+			fontFamily:'InstrumentSans',
+			fontWeight:'600',
+		},
+		buttonStyle:
+		{
+			flex:0,
+			backgroundColor: '#5db075',
+			width: '80%',
+			alignItems:'center',
+			justifyContent: 'center',
+			height: 40,
+			borderRadius: 10,
+		},
+		buttonText:
+		{
+			fontFamily:'InstrumentSans',
+			fontSize:16,
+			fontWeight:'700',
 		}
   }
 )
--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,43 +1,66 @@
 import { Text, View, StyleSheet, ScrollView, Button, Pressable} from "react-native";
 import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import {Link} from 'expo-router'
+import {
+  SourceSerifPro_400Regular,
+} from '@expo-google-fonts/source-serif-pro'
 
 
 export default function RecipeGeneration() {
 
+  	let [fontsLoaded] = useFonts({
+		SourceSerifPro_400Regular,
+    'InstrumentSans': require('../assets/fonts/InstrumentSans.ttf'),
+    'InstrumentSans-italics': require('../assets/fonts/InstrumentSans-Italic.ttf'),
 
+	});
   
-  const testList = [{recipe_Name:'bacon egg and cheese', ingredients: "testing ingredients \ningredients 1 \ningredients 2 \ningredients 3", directions: ["dir test 1", "dir test 2"]}];
+  
+  const testList = [{recipe_Name:'bacon egg and cheese', ingredients: "testing ingredients \ningredients 1 \ningredients 2 \ningredients 3", directions: ["dir test 1", "dir test 2"]},
+  {recipe_Name:'isaac  sucks', ingredients:'isaac sucks', directions: ['isaac sucks','issac sucks']},
+  {recipe_Name:'bacon egg and cheese', ingredients: "testing ingredients \ningredients 1 \ningredients 2 \ningredients 3", directions: ["dir test 1", "dir test 2"]},
+  {recipe_Name:'bacon egg and cheese', ingredients: "testing ingredients \ningredients 1 \ningredients 2 \ningredients 3", directions: ["dir test 1", "dir test 2"]}          
+  ];
 
-  return (
-    <View style = {style.pageTemp}>
-      <View style = {style.topHeader}>
-        <Text numberOfLines={2} style = {style.titleStyle}> Recipe{"\n"}Generation</Text>
-      </View>
+  if(!fontsLoaded)
+  {
+    return <AppLoading/>
+  }else
+    {
+    return (
+      <ScrollView>
+      <View style = {style.pageTemp}>
+        <View style = {style.topHeader}>
+          <Text numberOfLines={2} style = {style.titleStyle}> Recipe{"\n"}Generation</Text>
+        </View>
 
-        <Pressable style={style.buttonStyle} onPress={() => console.log("for backend")}>
-          <Text style={style.buttonText}> Click to Generate! </Text>
-        </Pressable>
-      <View style = {style.listStyle}>
-      {
-        testList.map((item,i)=>
-        {
-          //finish onClick
-         return <Link asChild href={{ 
-          pathname: './RecipePage',
-          params: {recipe_Name:item.recipe_Name, ingredients:item.ingredients, directions:item.directions},
-         }}>
-         <Pressable style = {style.recipeList} key={i}> 
-          <Text style = {style.recipeTitle}>{item.recipe_Name} </Text>
-          <Text style = {style.recipeIngredients}>{item.ingredients}</Text>
-          <Text style = {style.firstDirection}>{item.directions}</Text>
+          <Pressable style={style.buttonStyle} onPress={() => console.log("for backend")}>
+            <Text style={style.buttonText}> Click to Generate! </Text>
           </Pressable>
-          </Link>
-        })
-}
+
+        <View style = {style.listStyle}>
+        {
+          testList.map((item,i)=>
+          {
+            //finish onClick
+          return <Link asChild href={{ 
+            pathname: './RecipePage',
+            params: {recipe_Name:item.recipe_Name, ingredients:item.ingredients, directions:item.directions},
+          }}>
+          <Pressable style = {style.recipeList} key={i}> 
+            <Text style = {style.recipeTitle}>{item.recipe_Name} </Text>
+            <Text style = {style.recipeIngredients}>{item.ingredients}</Text>
+            <Text style = {style.firstDirection}>{item.directions}</Text>
+            </Pressable>
+            </Link>
+          })
+          }
+        </View>
       </View>
-    </View>
-  );
+      </ScrollView>
+    );
+  }
 }
 
 const style = StyleSheet.create(
@@ -60,7 +83,7 @@ const style = StyleSheet.create(
     {
       fontSize: 36,
       fontWeight: '400',
-      fontFamily: 'SourceSerifPro'
+      fontFamily: 'SourceSerifPro_400Regular'
 
     },
     buttonStyle:
@@ -81,6 +104,7 @@ const style = StyleSheet.create(
     {
       flex : 0,
       color: '#FFFFFF',
+      fontFamily: 'InstrumentSans-italics'
     },
     listStyle:
     {
@@ -99,17 +123,24 @@ const style = StyleSheet.create(
     recipeTitle:
     {
       fontSize: 20,
-      margin: 3
+      margin: 3,
+      fontWeight:'600',
+      fontFamily: 'InstrumentSans'
     },
     recipeIngredients:
     {
       fontSize: 15,
-      margin: 1
+      margin: 1,
+      fontWeight:'600',
+      fontFamily: 'InstrumentSans'
+
     },
     firstDirection:
     {
       fontSize: 15,
-      margin: 3
+      margin: 3,
+      fontWeight:'600',
+      fontFamily: 'InstrumentSans'
     }
   }
 )
